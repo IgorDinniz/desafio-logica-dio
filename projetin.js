6df7dcbefa4e7f3fd3eb83af68802521060cf4e4
igorleonino1@outlook.com <115420846+IgorDinniz@users.noreply.github.com>
2023-11-12 10:43:27 -0300

    primeiro commit dio

---
+++ projetin.js
@@ -0,0 +1,22 @@
+ /* let pontosDeVida = 0
+
+for(let i = 1; i <= 10; i++) {
+    pontosDeVida += 1
+    console.log("tomou porcao magica " + i)
+}
+
+console.log(pontosDeVida + "totais ")
+
+let i = 0
+while(contador <= 3) {
+    console.log("ola")
+    i++
+}
+
+let i = 0
+do {
+console.log("olaa")
+i++
+} while (i < 3)
+    */
+console.log("ola mundooooo")
